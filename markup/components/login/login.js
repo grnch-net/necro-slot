60feafbfd10e375983017c8199a69cad3f5082fb
Gaboa <dmitry.zhabko@gmail.com>
2016-06-30 12:30:09 +0300

    Login Module

---
+++ markup/components/login/login.js
@@ -0,0 +1,48 @@
+let login = (function () {
+
+    // Consts
+    const serviceUrl = 'http://gameservice.bossgs.org/slot/SlotService.svc/';
+
+    // Flag
+    let logged = false;
+
+    function getSessionID(userID, casinoID) {
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                /* eslint-disable */
+                url: `${serviceUrl}_Login/${userID}/${casinoID}`,
+                /* eslint-enable */
+                dataType: 'JSONP',
+                type: 'GET',
+                success: resolve,
+                error: reject
+            });
+        });
+    }
+
+    function tryToLogin(userID, casinoID) {
+        getSessionID(userID, casinoID)
+            .then(sessionID => {
+                logged = true;
+                console.log(`I am logged! SessionID is ${sessionID}`);
+                /* eslint-disable */
+                events.trigger('logged', sessionID);
+                /* eslint-enable */
+            })
+            .catch(error => console.error(error));
+    }
+
+    function isLogged() {
+        if (logged) {
+            console.log('You are logged.');
+        } else {
+            console.log('I don\'t know you.');
+        }
+    }
+
+    return {
+        tryToLogin: tryToLogin,
+        isLogged: isLogged
+    };
+
+})();
