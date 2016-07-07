663a0afe3e367b6af6ec0b152d2d51ece4b09c0c
Gaboa <dmitry.zhabko@gmail.com>
2016-07-07 12:08:06 +0300

    Canvas Module

--- markup/components/canvas/canvas.js
+++ markup/components/canvas/canvas.js
@@ -1,1 +1,57 @@
+let canvas = (function () {
 
+    let stages;
+
+    function initStages() {
+        /* eslint-disable */
+        const bgStaticStage = new createjs.Stage('bgStaticCanvas').set({name: 'bgStaticStage'});
+        const gameStaticStage = new createjs.Stage('gameStaticCanvas').set({name: 'gameStaticStage'});
+        const bonusStaticStage = new createjs.Stage('bonusStaticCanvas').set({name: 'bonusStaticStage'});
+        const bgStage = new createjs.Stage('bgCanvas').set({name: 'bgStage'});
+        const gameStage = new createjs.Stage('gameCanvas').set({name: 'gameStage'});
+        const bonusStage = new createjs.Stage('bonusCanvas').set({name: 'bonusStage'});
+
+        createjs.Ticker.timingMode = createjs.Ticker.RAF;
+        createjs.Ticker.on('tick', bgStage);
+        createjs.Ticker.on('tick', gameStage);
+        createjs.Ticker.on('tick', bonusStage);
+
+        stages = {
+            bgStage,
+            bgStaticStage,
+            gameStage,
+            gameStaticStage,
+            bonusStage,
+            bonusStaticStage
+        };
+
+        events.trigger('stagesCreated', stages);
+        /* eslint-enable */
+    }
+
+    function getStages() {
+        if (stages) {
+            return stages;
+        } else {
+            throw new Error('Stages are not created.');
+        }
+    }
+
+    function clearStage(stage) {
+        /* eslint-disable */
+        stage.removeAllChildren();
+        stage.off("tick", stage.handleEvent);
+        createjs.Tween.removeTweens(stage);
+        console.log(`I have cleared ${stage.toString()}!`);
+        /* eslint-enable */
+    }
+
+    /* eslint-disable */
+    events.on('initCanvas', initStages);
+    /* eslint-enable */
+
+    return {
+        getStages,
+        clearStage
+    };
+})();
